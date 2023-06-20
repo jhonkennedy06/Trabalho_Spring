b0436bad47600d834794332b2f946ef14d2b5b99
Jhon <jhonkennedy.kennedy46@gmail.com>
2023-06-20 12:50:20 -0300

    spring criado
    foi criado o projeto spring

---
+++ src/main/java/com/edu/ifmg/TrabalhoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ifmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/ifmg/TrabalhoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ifmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
